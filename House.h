385fdb8fccac12ad8319de66a450e32b25b0db2e
GabrielGAR96 <gabriel_ribeiro1996@hotmail.com>
2019-03-06 18:40:05 -0500

    Update House.h

--- House.h
+++ House.h
@@ -11,18 +11,18 @@ using namespace std;
 #include "HouseColor.h"
 
 class House{
-private:
-    int price = 0;
+private: //data members
+    int price = 0; 
     HouseColor houseColor;
 
-public:
-    House();
-    House(HouseColor houseColor);
-    ~House();
+public: 
+    House(); 
+    House(HouseColor houseColor); //constructors
+    ~House(); //destructor
 
     int getPrice() const;
     HouseColor getHouseColor() const;
-    void setPrice(int price);
+    void setPrice(int price); //getters and setters
 };
 
 #endif //POWERGRID_HOUSE_H
